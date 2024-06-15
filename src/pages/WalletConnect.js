94f09a624d0608f924a47889d286931ec8450562
umarz317 <umar.317@gmail.com>
2024-06-15 21:27:42 +0500

    Update WalletConnect.js

--- src/pages/WalletConnect.js
+++ src/pages/WalletConnect.js
@@ -20,7 +20,7 @@ const WalletConnect = () => {
   });
 
   useAccountEffect({onDisconnect:()=>{refetch()}})
-  
+
   function formatNumber(num) {
     num = parseFloat(num);
     if(num > 999 && num < 1000000){
@@ -31,6 +31,9 @@ const WalletConnect = () => {
     }
     else if(num > 1000000000){
       return (num/1000000000).toFixed(1) + 'B';
+    }
+    else{
+      return num.toString()
     }
   }
 
